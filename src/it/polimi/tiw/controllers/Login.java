fe69baa1a9995c93ac5daeca5bf99b666d71ae5d
AndreaDonati <andrea3.donati@mail.polimi.it>
2021-03-27 19:24:55 +0100

    Refactoring Login servlet

--- src/it/polimi/tiw/controllers/Login.java
+++ src/it/polimi/tiw/controllers/Login.java
@@ -1,56 +1,124 @@
 package it.polimi.tiw.controllers;
 
 import java.io.IOException;
-import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class RequestController
- * This class serves the purpose of transforming the requests from
- * post to get in order to solve the issue with post and refresh
- * AND of course to log the user into the system
- */
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+//import org.apache.commons.lang.StringEscapeUtils;
+//import org.thymeleaf.TemplateEngine;
+//import org.thymeleaf.context.WebContext;
+//import org.thymeleaf.templatemode.TemplateMode;
+//import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import it.polimi.tiw.beans.User;
+import it.polimi.tiw.dao.UserDAO;
+import it.polimi.tiw.utils.ConnectionHandler;
+
 @WebServlet("/Login")
+@MultipartConfig
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private Connection connection = null;
+	private TemplateEngine templateEngine;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String usr = request.getParameter("usr"); 
-		String psw = request.getParameter("psw");
-		
-		String dbUsr = getServletContext().getInitParameter("dbUser");
-		String dbPsw = getServletContext().getInitParameter("dbPassword");
-		String dbURL = getServletContext().getInitParameter("dbUrl");
-		
-		try {
-			DriverManager.getConnection(dbURL, dbUsr, dbPsw);
-		} catch (Exception e) {
-			// TODO: handle exception
-		}
+	public Login() {
+		super();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	public void init() throws ServletException {
+		connection = ConnectionHandler.getConnection(getServletContext());
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// obtain and escape params
+		String usrn = null;
+		String pwd = null;
+		try {
+//			usrn = StringEscapeUtils.escapeJava(request.getParameter("username"));
+//			pwd = StringEscapeUtils.escapeJava(request.getParameter("pwd"));
+			usrn = request.getParameter("username");
+			pwd = request.getParameter("pwd");
+			System.out.println(usrn+" - "+pwd);
+			if (usrn == null || pwd == null || usrn.isEmpty() || pwd.isEmpty()) {
+				throw new Exception("Credenziali vuote o mancanti.");
+			}
+
+		} catch (Exception e) {
+			response.setStatus(400);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write("{\"errorMessage\":\"Credenziali vuote o mancanti.\"}");
+			return;
+		}
+
+		// query db to authenticate for user
+		UserDAO userDao = new UserDAO(connection);
+		User user = null;
+		try {
+			user = userDao.checkCredentials(usrn, pwd);
+		} catch (SQLException e) {
+			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.toString());
+			return;
+		}
+
+		// If the user exists, add info to the session and go to home page, otherwise
+		// show login page with error message
+		
+		// Controllo se lo user che si è loggato è uno studente o un professore e lo reindirizzo 
+		// alla home corrispondende allo studente o al professore
+		String path="";
+		if (user == null) {
+			response.setStatus(401);
+			response.setContentType("application/json");
+			response.setCharacterEncoding("UTF-8");
+			response.getWriter().write("{\"errorMessage\":\"Matricola o password non corretti.\"}");
+			return;
+//			ServletContext servletContext = getServletContext();
+//			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+//			ctx.setVariable("errorMsg", "Matricola o password non corretti.");
+//			path = "/index.html";
+//			templateEngine.process(path, ctx, response.getWriter());
+		} else {
+			request.getSession().setAttribute("user", user);
+			// seleziono il path corretto in base al ruolo dello user
+			if(user.getRuolo().equals("student")) 
+				path = getServletContext().getContextPath() + "/HomeStudente";
+			else if(user.getRuolo().equals("teacher")) 
+				path = getServletContext().getContextPath() + "/HomeProf";
+
+			response.sendRedirect(path);
+		} 
+	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
+	public void destroy() {
+		try {
+			ConnectionHandler.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
