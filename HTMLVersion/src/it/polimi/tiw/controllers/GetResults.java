785a81e03e7c47822e63ae0747c88344bfa18b0a
AndreaDonati <andrea3.donati@mail.polimi.it>
2021-04-01 11:52:39 +0200

    Added exams results html template and fixed servlet redirect

--- HTMLVersion/src/it/polimi/tiw/controllers/GetResults.java
+++ HTMLVersion/src/it/polimi/tiw/controllers/GetResults.java
@@ -5,12 +5,18 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import com.google.gson.Gson;
 
@@ -26,6 +32,7 @@ import it.polimi.tiw.utils.ConnectionHandler;
 @WebServlet("/getResults")
 public class GetResults extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private TemplateEngine templateEngine;
 	private Connection connection = null;
 
     /**
@@ -37,6 +44,12 @@ public class GetResults extends HttpServlet {
     }
     
 	public void init() throws ServletException {
+		ServletContext servletContext = getServletContext();
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+		templateResolver.setTemplateMode(TemplateMode.HTML);
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
 		connection = ConnectionHandler.getConnection(getServletContext());
 	}
 
@@ -89,16 +102,12 @@ public class GetResults extends HttpServlet {
 		// uso json.encode per codificare l'oggetto complesso da mandare 
 		// al client come risultatos
 		
-//		Gson gson = new Gson();
-//		
-//		String jsonObj = gson.toJson(risultati);
-//		System.out.println(jsonObj);
-		
-		response.setStatus(HttpServletResponse.SC_OK);
-		response.setContentType("application/json");
-		response.setCharacterEncoding("UTF-8");
-//		response.getWriter().write(jsonObj);
-		response.getWriter().write(risultati.toString());		
+		// Indirizza l'utente alla home e aggiunge corsi e corrispondenza corsi-esami ai parametri
+		String path = "/Templates/RisultatiEsame.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		ctx.setVariable("risultati", risultati);
+		templateEngine.process(path, ctx, response.getWriter());		
 	}
 
 	private List<Esaminazione> getRisultatiEsamiByUserRole(User user, int idEsame) throws SQLException{
