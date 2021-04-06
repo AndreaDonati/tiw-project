dca249ae64f06a45cc401d4c0db6a1f2a22d1fd7
gregalletti <gregorio.galletti@mail.polimi.it>
2021-04-06 21:05:17 +0200

    Spinner added, some new js functions added too

--- JSVersion/WebContent/js/homeJS.js
+++ JSVersion/WebContent/js/homeJS.js
@@ -1,4 +1,4 @@
-/* Navbar Animation onScroll */
+/* Animazione Navbar on Scroll */
 $(window).scroll(function(){
     if($(document).scrollTop() > 80){
       $('nav').addClass('animate');
@@ -11,51 +11,128 @@ $(window).scroll(function(){
     }
   });
 
-var request;
-
-function init() {
-// Rimpiazza la gestione del browser
-// chiamata a servlet che risponde con json dei corsi
-	console.log("Chiamo init");
-	makeGet(showCorsi); // Chiamata asincrona
-//	makeGet(riempiSidebar);
-}
-
-function makeGet(callback) {	
+/* Funzione per mandare richieste GET */
+function makeGet(servletUrl, callback) {	
 	// Richiesta asincrona non cambia la pagina
 	request = new XMLHttpRequest(); // Nuova richiesta
-	var url = 'getCorsi'; // URL della Servlet
+	// var url = 'getCorsi'; // URL della Servlet
 
 	request.onreadystatechange = callback; // Chiamata al callback
-	request.open("GET", url); // Richiesta POST all'URL
+	request.open("GET", servletUrl); // Richiesta POST all'URL
 	request.send();
 }
 
+var request;
+var loaderDiv;
 
-function showCorsi() {
+function init() {
+	loaderDiv = document.getElementById("loader");
+// Rimpiazza la gestione del browser
+// chiamata a servlet che risponde con json dei corsi
+	console.log("Chiamo init");
+	// QUI INIZIO LOADER
+	loaderDiv.style.display = "block";
+
+	// QUI CHIAMATA PER PRENDERE DATI SESSION
+	//makeGet("getInfoUtente", showInfo);
+	makeGet("getCorsi", showCorsi); // Chiamata asincrona
+//	makeGet(riempiSidebar);
+}
+
+function showInfo() {
     // Se 400 (Bad request) o 401 (Unauthorized) loggo l'errore
 	if (request.readyState == 4 && (request.status == 400 || request.status == 401)) 
 	 	console.log(JSON.parse(request.responseText)["errorMessage"]);
 
-    // Se 200 (OK) reindirizzo l'utente alla home (mandata con un sendRedirect dalla Servlet)
+    // Se 200 (OK) sostituisco informazioni utente nella navbar
+	if (request.readyState == 4 && request.status == 200 ) {
+		utente = JSON.parse(request.responseText);
+		$("#immagine").attr("src", utente.immagine);
+		$("#nome").text(utente.nome); //nome e cognome
+		$("#mail").text(utente.mail);
+		$("#matricola").text(utente.matricola);
+		
+	}
+}
+
+function showCorsi() {
+	// QUI FINE LOADER   
+	loaderDiv.style.display = "none";
+
+    // Se 400 (Bad request) o 401 (Unauthorized) loggo l'errore
+	if (request.readyState == 4 && (request.status == 400 || request.status == 401)) 
+	 	console.log(JSON.parse(request.responseText)["errorMessage"]);
+
+    // Se 200 (OK) append dei corsi ricevuti al div corrispondente
 	if (request.readyState == 4 && request.status == 200 ) {
 		corsi = JSON.parse(request.responseText);
-		$("#content").append('<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true"></div>');
+		$("#content").append(
+		'<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">'+
+		'</div>');
 		for (i=0; i<corsi.length; i++){
-			$("#accordion").append('<div class="panel panel-default">'+
-                        '<div class="panel-heading" role="tab" id="headingOne">'+
-                           '<h4 class="panel-title">'+
-                                '<a data-parent="#accordion" onclick="getEsami(`'+corsi[i].nome+'`)" >'+corsi[i].nome+'</a>'+
-                            '</h4>'+
-                        '</div>'+
-                    '</div>');
+			$("#accordion").append(
+				'<div class="panel panel-default">'+
+                '	<div class="panel-heading" role="tab" id="headingOne">'+
+                '		<h4 class="panel-title">'+
+                '			<a data-parent="#accordion" onclick="getEsami(`'+corsi[i].nome+'`)" >'+corsi[i].nome+'</a>'+
+                '		</h4>'+
+                '	</div>'+
+                '</div>'
+				);
 		}
-		
-		
-		console.log(JSON.parse(request.responseText));
 	}
 }
 
 function getEsami(nomeCorso) {
 	console.log(nomeCorso);
+	$("#content").empty(); // rimuovo gli elementi che ci sono ora nella pagina, non servono piu
+	// QUI INIZIO LOADER
+	loaderDiv.style.display = "block";
+
+	makeGet("ElencoEsami", showEsami); // Chiamata asincrona
+}
+
+function showEsami() {
+	// QUI FINE LOADER  
+	loaderDiv.style.display = "none";
+
+    // Se 400 (Bad request) o 401 (Unauthorized) loggo l'errore
+	if (request.readyState == 4 && (request.status == 400 || request.status == 401)) 
+	 	console.log(JSON.parse(request.responseText)["errorMessage"]);
+
+    // Se 200 (OK) append di tutti i corsi + esami ricevuti al div corrispondente
+	if (request.readyState == 4 && request.status == 200 ) {
+		esami = JSON.parse(request.responseText);
+		// accordion con corsi + esami
+	}
+}
+
+function getRisultati(idEsame) {
+	console.log(idEsame);
+	$("#content").empty(); // rimuovo gli elementi che ci sono ora nella pagina, non servono piu
+	// QUI INIZIO LOADER
+	loaderDiv.style.display = "block";
+
+	makeGet("getResults", showRisultati); // Chiamata asincrona
+}
+
+function showRisultati() {
+	// QUI FINE LOADER  
+	loaderDiv.style.display = "none";
+
+    // Se 400 (Bad request) o 401 (Unauthorized) loggo l'errore
+	if (request.readyState == 4 && (request.status == 400 || request.status == 401)) 
+	 	console.log(JSON.parse(request.responseText)["errorMessage"]);
+
+    // Se 200 (OK) append di tutti i corsi + esami ricevuti al div corrispondente
+	if (request.readyState == 4 && request.status == 200 ) {
+		risultati = JSON.parse(request.responseText);
+		if(risultati.ruoloUtente == "teacher"){
+			console.log("we prof");
+			// tabella con tutti i risultati
+		} else {
+			console.log("we student");
+			// tabella con una sola riga
+		}
+	}
 }
